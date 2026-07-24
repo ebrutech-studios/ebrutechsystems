924471faf8475dcec0c9d425aa369387476a3778
EbruTech Studios <ebrutechstudios@gmail.com>
2026-07-24 18:57:39 +0300

    fix EbruLife record forms and persistence

--- life/service-worker.js
+++ life/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='ebrulife-v1.0.0';
+const CACHE='ebrulife-v1.1.0';
 const CORE=['/','/index.html','/app.html','/gorevler.html','/aliskanliklar.html','/hedefler.html','/butce.html','/abonelikler.html','/ozel-gunler.html','/ev-garanti.html','/araclar.html','/notlar.html','/ayarlar.html','/offline.html','/assets/css/life.css','/assets/js/app.js','/assets/js/store.js','/assets/js/config.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('ebrulife-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
